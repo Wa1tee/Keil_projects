e72848970dbe0a996afec42b35118771cbd49953
Wa1tee <antti.auranen@iki.fi>
2019-07-15 22:11:25 +0300

    added progress in learning pointers and call stack

--- blink_nucleo64/main.c
+++ blink_nucleo64/main.c
@@ -1,6 +1,9 @@
 
 #include<stdint.h>
+#include<stdbool.h>
+
 #include "Board_LED.h"
+#include "Board_Buttons.h"
 
 void delay (void)
 {
@@ -8,18 +11,48 @@ void delay (void)
 	for( i = 0; i< 500000; i++);
 }
 
+void pointers3(void){
+	void (*jump_addr) (void) = 0x00000000;
+	jump_addr();
+}
+
+void pointers2(void){
+	pointers3();
+}
+
+void pointers(void){
+	pointers2();
+}
+
 
 int main(void){
 	
+	bool ledstatus = false;
+	bool lastBtn = false;
+	LED_Initialize();
+	Buttons_Initialize();
+	
 	while(1)
 	{
-		LED_Initialize();
-		LED_On(0);
-		delay();
-		LED_Off(0);
-		delay();
-		delay();
-		delay();
+		pointers();
+		if(Buttons_GetState() ==1){
+			
+			//lastBtn = true;
+			if(ledstatus){
+				ledstatus = false;
+				delay();
+			}else{
+				ledstatus = true;
+				delay();
+			}
+		}
+		
+		if(ledstatus){
+			LED_On(0);
+			
+		}else{
+			LED_Off(0);
+		}
 	}
 	return 0;
 }
